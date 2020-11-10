df74cadbb6c4c8515d5e733700c2497d9f792d0b
Cat Meza <musical_kitty@hotmail.com>
2020-11-09 22:39:56 -0700

    updated db to use .env

--- db.js
+++ db.js
@@ -1,15 +1,9 @@
+require('dotenv').config();
+
 const { Client } = require("pg");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///text_test";
-} else {
-  DB_URI = "postgres://tvccnhwylotwje:28b489e732f91d27307b6052454ba9732d75de573108bb8b9ab11a2b22e9dfae@ec2-34-235-108-68.compute-1.amazonaws.com:5432/dbrqsrpqrb8d14";
-}
-
 let db = new Client({
-  connectionString: DB_URI
+  connectionString: process.env.DATABASE_URL
 });
 
 db.connect();
