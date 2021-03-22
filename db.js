88c84d5f3e2de157f19ef044c8eecb0c76fe81df
Cat Meza <musical_kitty@hotmail.com>
2021-03-22 12:13:20 -0600

    fix ssl error

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const { Client } = require("pg");
 
 let db = new Client({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect();
